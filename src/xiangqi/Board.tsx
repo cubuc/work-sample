6c4bae1ec43295dfb7c35c540c82718fa5fbc2ce
Gero Birkhoelzer <gero.birkhoelzer@uni-konstanz.de>
2022-04-10 20:57:32 +0200

    remove unused code in Board

--- src/xiangqi/Board.tsx
+++ src/xiangqi/Board.tsx
@@ -3,9 +3,6 @@ import { GRID_SIZE } from "./Constants";
 import BoardLines from "./BoardLines";
 import BoardLogic from "./BoardLogic";
 
-const xAxis = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
-const yAxis = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 const Board = () => {
     const cellSize = GRID_SIZE;
 
